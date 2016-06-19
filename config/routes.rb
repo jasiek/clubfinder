0221de4ca338793d73870ca70c8e734d4f7587c2
Jan Szumiec <jan.szumiec@gmail.com>
2016-06-19 23:26:35 +0100

    Inital view.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
   resources :clubs
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  # Serve websocket cable requests in-process
-  # mount ActionCable.server => '/cable'
+  root to: 'welcome#index'
 end
